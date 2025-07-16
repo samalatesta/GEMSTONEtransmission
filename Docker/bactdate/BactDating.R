c03a96efcfba225619efd64b426a315d9689c6db
samalatesta <samalate@bu.edu>
2025-07-15 22:04:22 -0400

    make docker image for bactdate

--- Docker/bactdate/BactDating.R
+++ Docker/bactdate/BactDating.R
@@ -1,40 +1,112 @@
+###Author: Samantha Malatesta
+###Date: 07/15/2025
+###Email: smalates@broadinstitute.org
+###Purpose: Rscript for BactDating 
+###Inputs: Gubbins files, mcmc iters, id and date arrays, model specification
+
 # Open log file
 log_conn <- file("BactDate.log", "w")
 sink(log_conn, type = "output")
 sink(log_conn, type = "message")
 
-#install and load packages
-install.packages("devtools")
-devtools::install_github("xavierdidelot/BactDating")
-install.packages("ape")
+#record date and time 
+Sys.time()
 
+#Command Args
+args <- commandArgs(trailingOnly = TRUE)
+model_arg <- args[1]
+ids <- args[2]
+dates <- args[3]
+mcmc_arg <- args[4]
+file1 <- args[5]
+file2 <- args[6]
+file3 <-  args[7]
+file4 <- args[8]
+file5 <- args[9]
+
+print("Check args correct")
+model_arg
+ids
+dates
+mcmc_arg
+file1
+file2
+file3
+file4
+file5
+
+#install and load packages
+#install.packages("devtools")
+#devtools::install_github("xavierdidelot/BactDating")
+#install.packages("ape")
 
 library(BactDating)
 library(ape)
+library(dplyr)
 
 #change file extensions to make BactDating happy
 files <- list.files(pattern="*.nwk")
+print(files)
 newfiles <- gsub(".nwk$", ".tre", files)
 file.rename(files, newfiles)
 
 #needs .csv even though tab delimited in source code
 files <- list.files(pattern="*.tsv")
+print(files)
 newfiles <- gsub(".tsv$", ".csv", files)
 file.rename(files, newfiles)
 
 #prefix for Gubbins 
 prefix = sub("\\..*", "", list.files(pattern="*polymorphic_sites.fasta"))
+print(paste0("Prefix is ", prefix))
+
+#make df of dates and ids
+ids_temp = read.table("ids.txt")
+dates_temp = read.table("dates.txt")
+meta = cbind(ids_temp, dates_temp)
+colnames(meta) = c("id", "date")
+meta$date <- as.Date(meta$date)
+print("IDs and dates")
+head(meta)
 
 #load Gubbins output
 t=loadGubbins(prefix)
+print("Gubbins loaded")
 
 #remove reference tip
 t <- drop.tip(t, tip = "Reference")
 
+meta_sub <- meta %>% filter(id %in% t$tip.label) %>% select(id, date) %>% mutate(d=as.numeric(date)/365)
+
+#make date vector
+d=meta_sub$d
+
+#make names of d the id names
+names(d) <-meta_sub$id
+
+#run bactdate
+res=bactdate(t,d,nbIts = mcmc_arg, showProgress = F, useRec = T, model=model_arg)
+print("BactDate run complete")
+
 #plot input tree and save
-png(paste0(prefix, "_input_tree.png"))
+pdf(paste0( prefix, "_gubbins_tree.pdf"))
 plot(t)
 dev.off()
+
+#plot trace and save
+pdf(paste0( prefix, "_trace.pdf"))
+plot(res, type="trace")
+dev.off()
+
+#plot dated tree
+pdf(paste0( prefix, "_bactdate_tree.pdf"))
+plot(res, type="tree")
+dev.off()
+
+#nwk for dated tree
+write.tree(res$tree,paste0( prefix, "_dated_tree.nwk") )
+
+print("Output saved")
 
 # Close log file
 sink(type = "output")
--- workflows/wf_BactDate.wdl
+++ workflows/wf_BactDate.wdl
@@ -9,15 +9,12 @@ input {
     File gubbins_node_labelled_final_tree
     File gubbins_per_branch_statistics
     File gubbins_recombination_predictions
-    File Rscript
     Int mcmc
     String model
 
   }
     command <<<
-    # Copy the R script and input data (if provided) to the task's working directory
-    cp ~{Rscript} .
-    
+
     cp ~{gubbins_filtered_polymorphic_sites} .
     cp ~{gubbins_final_tree} .
     cp ~{gubbins_node_labelled_final_tree} .
@@ -30,7 +27,7 @@ input {
     declare -a bash_array=(~{sep=" " sample_dates})
     printf "%s\n" "${bash_array[@]}" > dates.txt
 
-        Rscript BactDating.R "~{model}" \
+        Rscript /app/BactDating.R "~{model}" \
            "ids.txt" \
             "dates.txt" \
             ~{mcmc} \
@@ -52,7 +49,7 @@ input {
             
         }
   runtime {
-    docker: "rocker/r-ver:latest" 
+    docker: "samalate/bactdate:latest" 
   }
 }
 
@@ -67,13 +64,12 @@ workflow BactDate {
     File gubbins_node_labelled_final_tree
     File gubbins_per_branch_statistics
     File gubbins_recombination_predictions
-    File Rscript
     Int mcmc
     String model
   }
 
   call BactDate_script{
-    input: ids=ids, sample_dates=sample_dates, model=model, mcmc=mcmc, gubbins_filtered_polymorphic_sites = gubbins_filtered_polymorphic_sites, gubbins_final_tree=gubbins_final_tree, gubbins_node_labelled_final_tree=gubbins_node_labelled_final_tree, gubbins_per_branch_statistics=gubbins_per_branch_statistics, gubbins_recombination_predictions=gubbins_recombination_predictions, Rscript=Rscript
+    input: ids=ids, sample_dates=sample_dates, model=model, mcmc=mcmc, gubbins_filtered_polymorphic_sites = gubbins_filtered_polymorphic_sites, gubbins_final_tree=gubbins_final_tree, gubbins_node_labelled_final_tree=gubbins_node_labelled_final_tree, gubbins_per_branch_statistics=gubbins_per_branch_statistics, gubbins_recombination_predictions=gubbins_recombination_predictions
   }
 
   output {
